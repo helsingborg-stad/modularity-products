23f6275f722b1e952bd01a599b76213d57e2ab53
GitHub Action <>
2023-09-26 12:49:07 +0000

    build: update package file versions to 1.0.0

--- modularity-products.php
+++ modularity-products.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Modularity Products
  * Plugin URI:        https://github.com/helsingborg-stad/modularity-products
  * Description:       An addon for displaying products.
- * Version:           1.0.0
+ * Version: 1.0.0
  * Author:            Freya Liljeberg
  * Author URI:        https://github.com/helsingborg-stad
  * License:           MIT
