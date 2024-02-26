49a029d62a39d598a16d7d94f44843a19b020f9d
Thor Brink <thor.brink@helsingborg.se>
2024-02-26 07:33:44 +0000

    fix: replace global blade service

--- source/php/App.php
+++ source/php/App.php
@@ -14,9 +14,6 @@ class App
 
         //Register module
         add_action('plugins_loaded', array($this, 'registerModule'));
-
-        // Add view paths
-        add_filter('Municipio/blade/view_paths', array($this, 'addViewPaths'), 1, 1);
     }
 
     /**
@@ -31,23 +28,5 @@ class App
                 'Products'
             );
         }
-    }
-
-    /**
-     * Add searchable blade template paths
-     * @param array  $array Template paths
-     * @return array        Modified template paths
-     */
-    public function addViewPaths($array)
-    {
-        // If child theme is active, insert plugin view path after child views path.
-        if (is_child_theme()) {
-            array_splice($array, 2, 0, array(MODULARITY_PRODUCTS_VIEW_PATH));
-        } else {
-            // Add view path first in the list if child theme is not active.
-            array_unshift($array, MODULARITY_PRODUCTS_VIEW_PATH);
-        }
-
-        return $array;
     }
 }
